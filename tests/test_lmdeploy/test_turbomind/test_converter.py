oliveagle/lmdeploy

commit 71eeeca7ecf6462d43aa9cf6c81f616433438686

Refactor converter about get_input_model_registered_name and get_output_model_registered_name_and_config (#1702)
* refector input_model_name and output_model_name

* udpate session_len

* update turbomind.py

* update

* fix get_input_model_registered_name

* fix config.update

* update docstring

* add ut for converter

* remove unused code

* move get_model_arch to archs.py

* update ut

* update turbomind model config by user-input engine config when deploying converted tm model

* fix get max_len from turbomind config

* update

* w4-plora -> plora-w4

* delete export_turbomind.py

* update prefill config

* use input session_len

---------

Co-authored-by: irexyc <irexyc@gmail.com>

lmdeploy/turbomind/deploy/target_model/base.py
@@ -14,7 +14,6 @@ from mmengine import Registry
 from pydantic.dataclasses import dataclass
 
 from lmdeploy.messages import TurbomindEngineConfig
-from lmdeploy.model import MODELS
 
 from ..source_model.base import BaseInputModel, BaseReader
 
@@ -35,7 +34,9 @@ def tprint(*args, **kwargs):
 @dataclass
 class TurbomindModelConfig:
     """Config for turbomind model."""
-    model_name: str = None
+
+    model_name: str = ''
+    model_arch: str = None
     tensor_para_size: int = None
     head_num: int = None
     kv_head_num: int = None
@@ -47,7 +48,7 @@ class TurbomindModelConfig:
     start_id: int = None
     end_id: int = None
     session_len: int = None
-    weight_type: str = 'fp16'
+    weight_type: str = None
     rotary_embedding: int = 128
     rope_theta: float = 10000.0
     size_per_head: int = 128
@@ -96,14 +97,19 @@ class TurbomindModelConfig:
         env['tensor_para_size'] = env['tp']
         ret = TurbomindModelConfig.from_dict(env, allow_none=True)
         ret.rotary_embedding = ret.size_per_head
-        if config.max_prefill_token_num is not None and \
-                config.session_len is not None and \
-                config.num_tokens_per_iter == 0:
-            ret.num_tokens_per_iter = config.max_prefill_token_num
-            ret.max_prefill_iters = (config.session_len +
-                                     config.max_prefill_token_num -
-                                     1) // config.max_prefill_token_num
         return ret
+
+    def update_prefill_config(self, config: TurbomindEngineConfig):
+        """Update the attributes related to split&fuse."""
+        if config.session_len is not None:
+            self.session_len = config.session_len
+        assert self.session_len is not None
+        if config.max_prefill_token_num is not None and \
+                config.num_tokens_per_iter == 0:
+            self.num_tokens_per_iter = config.max_prefill_token_num
+            self.max_prefill_iters = (self.session_len +
+                                      config.max_prefill_token_num -
+                                      1) // config.max_prefill_token_num
 
     def toini(self):
         config = copy.deepcopy(self.__dict__)
@@ -125,6 +131,21 @@ class TurbomindModelConfig:
             if v is None:
                 return False
         return True
+
+    def update(self, other: 'TurbomindModelConfig') -> None:
+        """Update the attributes of this instance with the attributes from
+        another instance.
+
+        Args:
+            other (TurbomindModelConfig): The instance from which to copy
+                attributes.
+        """
+        # Iterate over the fields of 'self'
+        for field_name, _ in self.__dataclass_fields__.items():
+            # If the field value in 'other' is not None,
+            # update the corresponding field in 'self'
+            if getattr(other, field_name) is not None:
+                setattr(self, field_name, getattr(other, field_name))
 
 
 def _weight_dtype_map(weight_type: str, default=None):
@@ -163,12 +184,9 @@ class BaseOutputModel(ABC):
     def get_config(self, cfg: TurbomindModelConfig) -> TurbomindModelConfig:
         """Generate turbomind model config (config.ini)."""
         _, bos_id, eos_id = self.input_model.tokenizer_info()
-        model_info = self.input_model.model_info()
-        session_len = model_info.get('max_position_embeddings',
-                                     MODELS.get(cfg.model_name)().session_len)
+
         final_cfg = cfg.__dict__
-        final_cfg.update(
-            dict(start_id=bos_id, end_id=eos_id, session_len=session_len + 8))
+        final_cfg.update(dict(start_id=bos_id, end_id=eos_id))
         final_cfg.update(self.input_model.model_info())
 
         # head_num, vocab_size

lmdeploy/turbomind/turbomind.py
@@ -20,11 +20,12 @@ from lmdeploy.model import (MODELS, BaseModel, ChatTemplateConfig,
 from lmdeploy.tokenizer import Tokenizer
 from lmdeploy.utils import _stop_words, get_logger, get_model
 
-from .deploy.converter import (get_model_format, supported_formats,
-                               update_config_weight_type, update_output_format)
+from .deploy.converter import (SUPPORTED_FORMATS,
+                               get_input_model_registered_name,
+                               get_output_model_registered_name_and_config)
 from .deploy.source_model.base import INPUT_MODELS
 from .deploy.target_model.base import OUTPUT_MODELS, TurbomindModelConfig
-from .supported_models import SUPPORTED_ARCHS, get_model_arch, is_supported
+from .supported_models import is_supported
 from .utils import ModelSource, get_model_source
 
 # TODO: find another way import _turbomind
@@ -225,8 +226,8 @@ class TurboMind:
         """Load model which is in hf format."""
         assert model_source == ModelSource.HF_MODEL, \
             f'{model_source} is not supported'
-        assert engine_config.model_format in supported_formats, \
-            f'The model format should be in {supported_formats}'
+        assert engine_config.model_format in SUPPORTED_FORMATS, \
+            f'The model format should be in {SUPPORTED_FORMATS}'
 
         # update model_format if not supplied and outputs_stats.pth exists
         if osp.exists(osp.join(model_path, 'outputs_stats.pth')) and \
@@ -238,53 +239,36 @@ class TurboMind:
             'Plz try pytorch engine instead.')
 
         # convert transformers model into turbomind model format
-        model_arch, _ = get_model_arch(model_path)
-        data_type = 'fp16'
-        output_format = 'fp16'
-        inferred_model_format = get_model_format(SUPPORTED_ARCHS[model_arch],
-                                                 engine_config.model_format)
-        cfg = TurbomindModelConfig.from_engine_config(engine_config)
         match_name = best_match_model(model_path)
-        # for session len
-        cfg.model_name = match_name \
-            if match_name is not None else 'base'
-        if inferred_model_format.find('awq') != -1:
-            cfg.weight_type = 'int4'
-            output_format = 'w4'
-            data_type = 'int4'
-            cfg.group_size = 128
-            if inferred_model_format == 'xcomposer2-awq':
-                output_format = 'plora-w4'
-        else:
-            output_format = update_output_format(cfg.model_name,
-                                                 inferred_model_format,
-                                                 model_path, output_format)
-            data_type = output_format
-            update_config_weight_type(output_format, cfg)
-            if inferred_model_format == 'xcomposer2':
-                output_format = 'plora'
-
-        input_model = INPUT_MODELS.get(inferred_model_format)(
+        input_model_name = get_input_model_registered_name(
+            model_path, engine_config.model_format)
+        input_model = INPUT_MODELS.get(input_model_name)(
             model_path=model_path, tokenizer_path=model_path, ckpt_path=None)
 
-        output_model = OUTPUT_MODELS.get(output_format)(
-            input_model=input_model, cfg=cfg, to_file=False, out_dir='')
+        output_model_name, cfg = get_output_model_registered_name_and_config(
+            model_path=model_path,
+            model_format=engine_config.model_format,
+            group_size=0)
+        self.config = TurbomindModelConfig.from_engine_config(engine_config)
+        self.config.update(cfg)
+        output_model = OUTPUT_MODELS.get(output_model_name)(
+            input_model=input_model,
+            cfg=self.config,
+            to_file=False,
+            out_dir='')
 
-        cfg = output_model.cfg
-        if engine_config.session_len is not None:
-            cfg.session_len = engine_config.session_len
-
-        self.model_name = cfg.model_name
-        self.config = cfg
-        self.data_type = data_type
-
-        logger.warning(f'model_config:\n\n{cfg.toini()}')
+        self.config = output_model.cfg
+        self.config.update_prefill_config(engine_config)
+        self.config.model_name = match_name \
+            if match_name is not None else 'base'
+        self.model_name = self.config.model_name
+        logger.info(f'model_config:\n\n{self.config.toini()}')
 
         model_comm = _tm.AbstractTransformerModel.create_llama_model(
             model_dir='',
-            config=cfg.toini(),
+            config=self.config.toini(),
             tensor_para_size=self.gpu_count,
-            data_type=data_type)
+            data_type=self.config.weight_type)
 
         # create empty weight
         self._create_weight(model_comm)
@@ -326,14 +310,12 @@ class TurboMind:
         # update cfg
         if engine_config is not None:
             engine_config.tp = cfg.tensor_para_size
-            cfg = _update_tm_config(cfg, engine_config)
-            if engine_config.session_len is not None:
-                cfg.session_len = engine_config.session_len
+            _cfg = _update_tm_config(cfg, engine_config)
+            cfg.update(_cfg)
 
         # update cls
         self.config = cfg
         self.model_name = cfg.model_name
-        self.data_type = cfg.weight_type
 
         # create model
         logger.warning(f'model_config:\n\n{cfg.toini()}')
@@ -342,7 +324,7 @@ class TurboMind:
             model_dir=weight_dir,
             config=cfg.toini(),
             tensor_para_size=self.gpu_count,
-            data_type=self.data_type)
+            data_type=self.config.weight_type)
 
         # create weight and load params
         self._create_weight(model_comm)
@@ -353,7 +335,6 @@ class TurboMind:
             cls,
             pretrained_model_name_or_path: str,
             engine_config: TurbomindEngineConfig = None,
-            model_name: Optional[str] = None,
             model_format: Optional[str] = None,
             group_size: Optional[int] = None,
             tp: Optional[int] = None,
@@ -375,7 +356,6 @@ class TurboMind:
                       on huggingface.co, such as "internlm/internlm-chat-7b",
                       "Qwen/Qwen-7B-Chat ", "baichuan-inc/Baichuan2-7B-Chat"
                       and so on.
-            model_name (str): needed when pretrained_model_name_or_path is c)
             model_format (str): model format
             group_size (int): group size
             tp (int): tensor parallel size
@@ -383,7 +363,7 @@ class TurboMind:
                 Can be used to update configuration when initialize the engine.
         """
         model_source = get_model_source(pretrained_model_name_or_path)
-        logger.warning(f'model_source: {model_source}')
+        logger.info(f'model_source: {model_source}')
         return cls(model_path=pretrained_model_name_or_path,
                    engine_config=engine_config,
                    model_source=model_source,

tests/test_lmdeploy/test_turbomind/test_converter.py
@@ -1,0 +1,85 @@
+from lmdeploy import TurbomindEngineConfig
+from lmdeploy.turbomind.deploy.converter import (
+    get_input_model_registered_name,
+    get_output_model_registered_name_and_config)
+from lmdeploy.turbomind.deploy.source_model.base import INPUT_MODELS
+from lmdeploy.turbomind.deploy.target_model.base import (OUTPUT_MODELS,
+                                                         TurbomindModelConfig)
+
+
+def test_registered_models():
+    for model in [
+            'baichuan-inc/Baichuan-7B', 'baichuan-inc/Baichuan2-7B-Chat',
+            'baichuan-inc/Baichuan-13B-Chat',
+            'baichuan-inc/Baichuan2-13B-Chat', 'internlm/internlm-chat-7b',
+            'internlm/internlm2-chat-7b',
+            'internlm/internlm-xcomposer2-4khd-7b',
+            'internlm/internlm-xcomposer2-vl-7b',
+            'internlm/internlm-xcomposer2-7b', 'lmsys/vicuna-7b-v1.5',
+            '01-ai/Yi-1.5-9B', 'deepseek-ai/deepseek-coder-6.7b-instruct',
+            'deepseek-ai/deepseek-llm-7b-chat', 'Qwen/Qwen-7B-Chat',
+            'Qwen/Qwen1.5-7B-Chat', 'Qwen/Qwen-VL-Chat',
+            'liuhaotian/llava-v1.6-34b', 'liuhaotian/llava-v1.6-mistral-7b',
+            'liuhaotian/llava-v1.6-vicuna-13b', 'OpenGVLab/InternVL-Chat-V1-5',
+            'deepseek-ai/deepseek-vl-7b-chat', 'YanweiLi/MGM-7B'
+    ]:
+        input_name = get_input_model_registered_name(model, model_format='hf')
+        assert input_name in list(INPUT_MODELS.module_dict.keys())
+
+        output_name, config = get_output_model_registered_name_and_config(
+            model, model_format='hf', group_size=0)
+        assert output_name in list(OUTPUT_MODELS.module_dict.keys())
+        assert config.group_size == 0
+        assert config.session_len > 0
+        assert config.model_arch is not None
+
+    for model in [
+            'Qwen/Qwen1.5-4B-Chat-AWQ',
+            'solidrust/Meta-Llama-3-8B-Instruct-hf-AWQ'
+    ]:
+        name = get_input_model_registered_name(model, model_format='awq')
+        assert name in list(INPUT_MODELS.module_dict.keys())
+        output_name, config = get_output_model_registered_name_and_config(
+            model, model_format='awq', group_size=0)
+        assert output_name in list(OUTPUT_MODELS.module_dict.keys())
+        assert config.group_size == 128
+        assert config.session_len > 0
+        assert config.model_arch is not None
+
+
+def test_turbomind_model_config_udpate():
+    this = TurbomindModelConfig.from_dict({}, allow_none=True)
+    this.head_num = 100
+    this.weight_type = 'fp16'
+
+    engine_config = TurbomindEngineConfig(model_format='awq',
+                                          tp=2,
+                                          session_len=4000,
+                                          max_batch_size=100,
+                                          cache_max_entry_count=0.5,
+                                          quant_policy=8,
+                                          rope_scaling_factor=3.0,
+                                          use_logn_attn=True,
+                                          max_prefill_iters=64,
+                                          num_tokens_per_iter=256)
+    other = TurbomindModelConfig.from_engine_config(engine_config)
+    this.update(other)
+
+    assert (this.head_num == 100)
+    assert (this.weight_type == 'fp16')
+    assert (this.tensor_para_size == other.tensor_para_size)
+    assert (this.session_len == other.session_len)
+    assert (this.max_batch_size == other.max_batch_size)
+    assert (this.cache_max_entry_count == other.cache_max_entry_count)
+    assert (this.max_prefill_iters == other.max_prefill_iters)
+    assert (this.num_tokens_per_iter == other.num_tokens_per_iter)
+    assert (this.quant_policy == other.quant_policy)
+    assert (this.rope_scaling_factor == other.rope_scaling_factor)
+    assert (this.use_logn_attn == other.use_logn_attn)
+
+    engine_config = TurbomindEngineConfig(max_prefill_iters=512,
+                                          num_tokens_per_iter=1024)
+    other = TurbomindModelConfig.from_engine_config(engine_config)
+    this.update(other)
+    assert (this.max_prefill_iters == other.max_prefill_iters)
+    assert (this.num_tokens_per_iter == other.num_tokens_per_iter)

lmdeploy/archs.py
@@ -2,6 +2,8 @@
 import os
 from typing import Literal, Optional, Union
 
+from transformers import AutoConfig
+
 from lmdeploy.utils import get_hf_config_content
 
 from .messages import PytorchEngineConfig, TurbomindEngineConfig
@@ -145,3 +147,43 @@ def get_task(model_path: str):
 
     # default task, pipeline_class
     return 'llm', AsyncEngine
+
+
+def get_model_arch(model_path: str):
+    """get a model's architecture and configuration.
+
+    Args:
+        model_path(str): the model path
+    """
+    if os.path.exists(os.path.join(model_path, 'triton_models', 'weights')):
+        # the turbomind model
+        import configparser
+        config_file = os.path.join(model_path, 'triton_models', 'weights',
+                                   'config.ini')
+        config = configparser.ConfigParser()
+        config.read(config_file)
+        model_arch = config['llama']['model_arch']
+        return model_arch, None
+    else:
+        # transformers model
+        try:
+            cfg = AutoConfig.from_pretrained(model_path,
+                                             trust_remote_code=True)
+        except Exception as e:  # noqa
+            from transformers import PretrainedConfig
+            cfg = PretrainedConfig.from_pretrained(model_path)
+
+        _cfg = cfg.to_dict()
+        if _cfg.get('architectures', None):
+            arch = _cfg['architectures'][0]
+            if _cfg.get('auto_map'):
+                for _, v in _cfg['auto_map'].items():
+                    if 'InternLMXComposer2ForCausalLM' in v:
+                        arch = 'InternLMXComposer2ForCausalLM'
+        elif _cfg.get('auto_map',
+                      None) and 'AutoModelForCausalLM' in _cfg['auto_map']:
+            arch = _cfg['auto_map']['AutoModelForCausalLM'].split('.')[-1]
+        else:
+            raise RuntimeError(
+                f'Could not find model architecture from config: {_cfg}')
+        return arch, cfg

commit afa6e3fef83659075921de87aedfdfee92a4d382

Fix param `--cache-max-entry-count` is not taking effect (#1758) (#1778)
* Fix param `--cache-max-entry-count` is not taking effect (#1758)

* fix side-effect caused by #1702

* update

---------

Co-authored-by: jack <jack@nss.org>
Co-authored-by: lvhan028 <lvhan_028@163.com>

lmdeploy/turbomind/deploy/target_model/base.py
@@ -91,18 +91,24 @@ class TurbomindModelConfig:
             default.update(used)
             return cls(**default)
 
-    @classmethod
-    def from_engine_config(cls, config: TurbomindEngineConfig):
-        env = copy.deepcopy(config.__dict__)
-        env['tensor_para_size'] = env['tp']
-        ret = TurbomindModelConfig.from_dict(env, allow_none=True)
-        ret.rotary_embedding = ret.size_per_head
-        return ret
+    def update_from_engine_config(self, config: TurbomindEngineConfig):
+        """Update the attributes of this instance with the attributes from
+        TurbomindEngineConfig.
 
-    def update_prefill_config(self, config: TurbomindEngineConfig):
-        """Update the attributes related to split&fuse."""
-        if config.session_len is not None:
-            self.session_len = config.session_len
+        Args:
+            config (TurbomindEngineConfig): The turbomind engine config
+        """
+        if config is None:
+            return
+        # Iterate over the fields of 'self'
+        for field_name, _ in self.__dataclass_fields__.items():
+            # If the field value in 'other' is not None,
+            # update the corresponding field in 'self'
+            if hasattr(config, field_name) and getattr(config,
+                                                       field_name) is not None:
+                setattr(self, field_name, getattr(config, field_name))
+
+        self.tensor_para_size = config.tp
         assert self.session_len is not None
         if config.max_prefill_token_num is not None and \
                 config.num_tokens_per_iter == 0:
@@ -131,21 +137,6 @@ class TurbomindModelConfig:
             if v is None:
                 return False
         return True
-
-    def update(self, other: 'TurbomindModelConfig') -> None:
-        """Update the attributes of this instance with the attributes from
-        another instance.
-
-        Args:
-            other (TurbomindModelConfig): The instance from which to copy
-                attributes.
-        """
-        # Iterate over the fields of 'self'
-        for field_name, _ in self.__dataclass_fields__.items():
-            # If the field value in 'other' is not None,
-            # update the corresponding field in 'self'
-            if getattr(other, field_name) is not None:
-                setattr(self, field_name, getattr(other, field_name))
 
 
 def _weight_dtype_map(weight_type: str, default=None):

lmdeploy/turbomind/turbomind.py
@@ -1,6 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import asyncio
-import copy
 import os.path as osp
 import sys
 from configparser import ConfigParser
@@ -78,21 +77,6 @@ def _update_engine_config(config: TurbomindEngineConfig, **kwargs):
         logger.warning('model_name is deprecated in TurbomindEngineConfig '
                        'and has no effect')
     return config
-
-
-def _update_tm_config(dst: TurbomindModelConfig, src: TurbomindEngineConfig):
-    if src.max_prefill_token_num is not None and \
-            src.session_len is not None and src.num_tokens_per_iter == 0:
-        dst.num_tokens_per_iter = src.max_prefill_token_num
-        dst.max_prefill_iters = (src.session_len + src.max_prefill_token_num -
-                                 1) // src.max_prefill_token_num
-    dst_dict = copy.deepcopy(dst.__dict__)
-    src_dict = copy.deepcopy(src.__dict__)
-    src_dict['tensor_para_size'] = src_dict['tp']
-    for k, v in src_dict.items():
-        if v is not None and k in dst_dict:
-            dst_dict[k] = v
-    return TurbomindModelConfig.from_dict(dst_dict)
 
 
 class TurboMind:
@@ -249,16 +233,11 @@ class TurboMind:
             model_path=model_path,
             model_format=engine_config.model_format,
             group_size=0)
-        self.config = TurbomindModelConfig.from_engine_config(engine_config)
-        self.config.update(cfg)
+        cfg.update_from_engine_config(engine_config)
         output_model = OUTPUT_MODELS.get(output_model_name)(
-            input_model=input_model,
-            cfg=self.config,
-            to_file=False,
-            out_dir='')
+            input_model=input_model, cfg=cfg, to_file=False, out_dir='')
 
         self.config = output_model.cfg
-        self.config.update_prefill_config(engine_config)
         self.config.model_name = match_name \
             if match_name is not None else 'base'
         self.model_name = self.config.model_name
@@ -307,11 +286,9 @@ class TurboMind:
             logger.info(f'found tp={cfg.tensor_para_size} in config.ini.')
             self.gpu_count = cfg.tensor_para_size
 
-        # update cfg
         if engine_config is not None:
             engine_config.tp = cfg.tensor_para_size
-            _cfg = _update_tm_config(cfg, engine_config)
-            cfg.update(_cfg)
+            cfg.update_from_engine_config(engine_config)
 
         # update cls
         self.config = cfg

tests/test_lmdeploy/test_turbomind/test_converter.py
@@ -3,8 +3,7 @@ from lmdeploy.turbomind.deploy.converter import (
     get_input_model_registered_name,
     get_output_model_registered_name_and_config)
 from lmdeploy.turbomind.deploy.source_model.base import INPUT_MODELS
-from lmdeploy.turbomind.deploy.target_model.base import (OUTPUT_MODELS,
-                                                         TurbomindModelConfig)
+from lmdeploy.turbomind.deploy.target_model.base import OUTPUT_MODELS
 
 
 def test_registered_models():
@@ -47,12 +46,40 @@ def test_registered_models():
         assert config.model_arch is not None
 
 
-def test_turbomind_model_config_udpate():
-    this = TurbomindModelConfig.from_dict({}, allow_none=True)
-    this.head_num = 100
-    this.weight_type = 'fp16'
+def test_update_from_engine_config():
+    import copy
+    _, _config = get_output_model_registered_name_and_config(
+        'internlm/internlm2-chat-7b', model_format='hf', group_size=0)
+    config = copy.deepcopy(_config)
+    config.update_from_engine_config(None)
+    assert (config == _config)
 
-    engine_config = TurbomindEngineConfig(model_format='awq',
+    config = copy.deepcopy(_config)
+    config.update_from_engine_config(TurbomindEngineConfig())
+    assert config.tensor_para_size == 1
+    assert config.session_len == 32776
+    assert config.max_batch_size == 128
+    assert config.cache_max_entry_count == 0.8
+    assert config.quant_policy == 0
+    assert config.max_prefill_iters == 5
+    assert config.num_tokens_per_iter == 8192
+
+    config = copy.deepcopy(_config)
+    config.update_from_engine_config(
+        TurbomindEngineConfig(max_prefill_token_num=2048,
+                              num_tokens_per_iter=0))
+    assert config.max_prefill_iters == 17
+    assert config.num_tokens_per_iter == 2048
+
+    config = copy.deepcopy(_config)
+    config.update_from_engine_config(
+        TurbomindEngineConfig(max_prefill_token_num=2048,
+                              num_tokens_per_iter=256))
+    assert config.max_prefill_iters == 1
+    assert config.num_tokens_per_iter == 256
+
+    config = copy.deepcopy(_config)
+    engine_config = TurbomindEngineConfig(model_format='hf',
                                           tp=2,
                                           session_len=4000,
                                           max_batch_size=100,
@@ -62,24 +89,16 @@ def test_turbomind_model_config_udpate():
                                           use_logn_attn=True,
                                           max_prefill_iters=64,
                                           num_tokens_per_iter=256)
-    other = TurbomindModelConfig.from_engine_config(engine_config)
-    this.update(other)
 
-    assert (this.head_num == 100)
-    assert (this.weight_type == 'fp16')
-    assert (this.tensor_para_size == other.tensor_para_size)
-    assert (this.session_len == other.session_len)
-    assert (this.max_batch_size == other.max_batch_size)
-    assert (this.cache_max_entry_count == other.cache_max_entry_count)
-    assert (this.max_prefill_iters == other.max_prefill_iters)
-    assert (this.num_tokens_per_iter == other.num_tokens_per_iter)
-    assert (this.quant_policy == other.quant_policy)
-    assert (this.rope_scaling_factor == other.rope_scaling_factor)
-    assert (this.use_logn_attn == other.use_logn_attn)
+    config.update_from_engine_config(engine_config)
 
-    engine_config = TurbomindEngineConfig(max_prefill_iters=512,
-                                          num_tokens_per_iter=1024)
-    other = TurbomindModelConfig.from_engine_config(engine_config)
-    this.update(other)
-    assert (this.max_prefill_iters == other.max_prefill_iters)
-    assert (this.num_tokens_per_iter == other.num_tokens_per_iter)
+    assert (config.tensor_para_size == engine_config.tp)
+    assert (config.session_len == engine_config.session_len)
+    assert (config.max_batch_size == engine_config.max_batch_size)
+    assert (
+        config.cache_max_entry_count == engine_config.cache_max_entry_count)
+    assert (config.quant_policy == engine_config.quant_policy)
+    assert (config.rope_scaling_factor == engine_config.rope_scaling_factor)
+    assert (config.use_logn_attn == engine_config.use_logn_attn)
+    assert (config.max_prefill_iters == engine_config.max_prefill_iters)
+    assert (config.num_tokens_per_iter == engine_config.num_tokens_per_iter)